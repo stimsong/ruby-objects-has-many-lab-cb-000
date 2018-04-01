39831e66e903b25a6f71107c52a3d9df12b9b02d
Greg Stimson <stimsong@yahoo.com>
2018-04-01 19:22:55 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -18,7 +18,7 @@ class Artist
     @@song_count += 1
   end
 
-  def add_song_by_name(name = "nil")
+  def add_song_by_name(name)
     song = Song.new(name)
     @songs << song
     song.artist = self
