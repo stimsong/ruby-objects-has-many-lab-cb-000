41322a998608f3d32dc0616ecd867f56640a8424
Greg Stimson <stimsong@yahoo.com>
2018-04-01 19:11:45 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,4 +1,5 @@
 class Post
+<<<<<<< HEAD
   attr_accessor :title, :author, :author_name
 
   def initialize(title = "nil")
@@ -7,4 +8,15 @@ class Post
     @author_name = author
   end
 
+=======
+  attr_accessor :title, :author_name, :author
+
+  def initialize(title)
+    @title = title
+    @author_name = author
+  end
+
+
+
+>>>>>>> a611761cbb51d09d7d84babee2cd87933e57af91
 end
