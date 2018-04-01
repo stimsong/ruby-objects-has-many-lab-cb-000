ba6ad21779a658361367d78857488dafccf13859
Greg Stimson <stimsong@yahoo.com>
2018-04-01 00:56:49 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,10 @@
+class Post
+  attr_accessor :title, :author, :author_name
+
+  def initialize(title = "nil")
+    @title = title
+    @author = author
+    @author_name = author
+  end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,11 @@
+class Song
+  attr_accessor :name, :artist
+
+  def initialize(name = "nil")
+    @name = name
+    @artist_name = name
+  end
+
+
+
+end
